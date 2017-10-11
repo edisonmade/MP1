3d876d7337a3b2324cf24c805a3c5f1681d40734
edisonmade <254495579a@gmail.com>
2017-10-10 21:29:42 -0400

    Initial commit

---
+++ MyStackTest.java
@@ -0,0 +1,112 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MyStackTest {
+
+
+	@Test 
+	public void testPush()
+	   {
+		MyStack<String> stack = new MyStack<String>();
+	      stack.push ("hello");
+	      stack.push ("world");
+	      assertEquals (stack.peek(), "world");
+	   }
+
+	@Test
+	public void testPop()
+	{
+		MyStack<String> stack = new MyStack<String>();
+		stack.push("bo");
+		stack.push("hi");
+		stack.pop();
+		assertEquals(stack.peek(), "bo");
+	}
+
+	@Test
+	public void testPush2()
+	{
+		MyStack<String> stack = new MyStack<String>();
+		stack.push("Bo");
+		stack.push("wang")
+		assertEquals(stack.peek(),"wang");
+	}
+
+	@Test
+	public void testPopEmpty()
+	{
+		MyStack<String> stack = new MyStack<String>();
+		stack.push("bo");
+		stack.push("hi");
+		stack.pop();
+		stack.pop();
+		assertEquals(stack.empty(), true);
+	}
+
+	@Test
+	public void testPopTwo()
+	{
+		MyStack<String> stack = new MyStack<String>();
+		stack.push("bo");
+		stack.push("wang");
+		stack.push("china");
+		stack.pop();
+		stack.pop();
+		assertEquals(stack.peek(), "bo");
+	}
+
+	@Test
+	public void testClearall()
+	{
+		MyStack <String> stack = new MyStack<String>();
+		stack.push("bo");
+		stack.push("wang");
+		stack.clear();
+		assertEquals(stack.size(), 0);
+	}
+
+
+	@Test
+	public void testSize()
+	{
+		MyStack<String> stack = new MyStack<String>();
+		stack.push("bo");
+		assertEquals(stack.size(), 1);
+	}
+
+	@Test
+	public void testEmpty() //check that stack is empty upon initialization
+	{
+		MyStack<String> stack = new MyStack<String>();
+		assertEquals(stack.size(), 0);
+	}
+
+	@Test
+	public void testPop()
+	{
+		MyStack <String> stack = new MyStack<String>();
+		stack.push("bo");
+		stack.pop();
+		assertEquals(stack.size(), 0);
+	}
+
+	@Test
+	public void testClear()
+	{
+		MyStack <String> stack = new MyStack<String>();
+		stack.push("bo");
+		stack.clear();
+		assertEquals(stack.size(), 0);
+	}	
+
+	@Test
+	public void testClearEmptyStack()
+	{
+		MyStack <String> stack = new MyStack<String>();
+		stack.clear();
+		assertEquals(stack.size(), 0);
+	}
+
+
+}
